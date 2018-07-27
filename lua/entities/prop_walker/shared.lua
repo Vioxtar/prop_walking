f2229ad4b1836af6cf057d8a7013c797891e5c1a
Havok <thehavok.hts@gmail.com>
2018-07-27 03:06:09 -0500

    First iteration of cleaning and optimising code
    I didnt test any of the changes because I did this at 3am

--- lua/entities/prop_walker/shared.lua
+++ lua/entities/prop_walker/shared.lua
@@ -1,70 +1,79 @@
-AddCSLuaFile()
+AddCSLuaFile( )
 
-DEFINE_BASECLASS("base_anim")
+DEFINE_BASECLASS( "base_anim" )
+
+local InactiveTimerDelay = 120
 
 --[[-------------------------------------------------------------------------
 Removal the prop walker after 2 minutes of inactivity
 ---------------------------------------------------------------------------]]
-function ENT:RemovalTimer()
-	if CLIENT then return end
-	timer.Adjust("pwRemove"..self:EntIndex(), 120, 1, function()
-		if IsValid(self) then self:Remove() end
-	end)
+function ENT:RestartRemovalTimer( )
+
+	return timer.Start( "pwRemove_".. self:EntIndex( ) )
+
 end
 
 -- A global table to find all prop walkers without iterating all entities
-propWalkers = {}
+propWalkers = propWalkers or {}
 
-local function HandleRemoval(pw)
-	propWalkers[pw] = nil
+local function HandleRemoval( pw )
+
+	timer.Remove( "pwRemove_".. self:EntIndex( ) )
+
+	table.remove( propWalkers, pw.PWIndex )
+
 end
 
 
 --[[-------------------------------------------------------------------------
 Init the prop walker and cover the ground
 ---------------------------------------------------------------------------]]
-function ENT:Initialize()
+function ENT:Initialize( )
 
-	local ground = self:GetNW2Entity("Ground")
-	if !IsValid(ground) then
-		if SERVER then self:Remove() end
+	local ground = self:GetNW2Entity( "Ground" )
+	if SERVER and !IsValid( ground ) then
+
+		self:Remove( )
+
 		return
+
 	end
 
 	-- Cover the new ground
-	self:RebuildPhysics(ground:GetModel())
-	self:SetPos(ground:GetPos())
-	self:SetAngles(ground:GetAngles())
-	self:SetModel(ground:GetModel())
-	self:SetParent(ground)
-	ground:SetNW2Entity("PropWalker", self)
+	self:RebuildPhysics( ground:GetModel( ) )
 
+	self:SetPos( ground:GetPos( ) )
+	self:SetAngles( ground:GetAngles( ) )
+	self:SetModel( ground:GetModel( ) )
+	self:SetParent( ground )
+
+	ground:SetNW2Entity( "PropWalker", self )
 	-- Set our original ground entity to collide with everything but the player himself
-	ground:SetCollisionGroup(COLLISION_GROUP_PASSABLE_DOOR)
-
-	-- Add the new prop walker to the table of prop walkers for quicker filtering in collision traces
-	propWalkers[self] = self
+	ground:SetCollisionGroup( COLLISION_GROUP_PASSABLE_DOOR )
+	-- Add the new prop walker to the table of prop walkers for quicker filtering in collision traces and store its index for removal
+	self.PWIndex = table.insert( propWalkers, self )
 	-- And remove ourselves from the table upon removal
-	self:CallOnRemove("HandleRemoval", HandleRemoval)
-
+	self:CallOnRemove( "HandleRemoval", HandleRemoval )
 	-- Make the prop walker invisible
-	self:DrawShadow(false)
-	self:SetNoDraw(true)
-
+	self:DrawShadow( false )
+	self:SetNoDraw( true )
 	-- Solidify the prop walker
-	self:SetSolid(SOLID_VPHYSICS)
-	self:SetCollisionGroup(COLLISION_GROUP_NONE)
-	self:EnableCustomCollisions(true)
+	self:SetSolid( SOLID_VPHYSICS )
+	self:SetCollisionGroup( COLLISION_GROUP_NONE )
+	self:EnableCustomCollisions( true )
 	
 	if SERVER then
 		-- Network our abs position as soon as possible for smooth player mounts
-		self:NetworkAbsPosition()
+		self:NetworkAbsPosition( )
 
-		-- Start a self removal timer in case of inactivity
-		self:RemovalTimer()
+		-- Create a self removal timer in case of inactivity
+		timer.Create( "pwRemove_".. self:EntIndex( ), InactiveTimerDelay, 1, function( )
 
+			SafeRemoveEntity( self )
+
+		end )
 		-- We want to use touch hooks to determine ground collision groups
-		-- self:SetTrigger(true)
+		-- self:SetTrigger( true )
 	end
 	
 end
@@ -73,21 +82,24 @@ end
 --[[-------------------------------------------------------------------------
 Rebuild the physics of the prop walker to match our ground's model
 ---------------------------------------------------------------------------]]
-function ENT:RebuildPhysics(model)
+function ENT:RebuildPhysics( model )
 
 	if self.PhysModel == model then return end -- We've already built the physics we want
 
 	self.PhysModel = model
-	self.PhysCollides = CreatePhysCollidesFromModel(model)
+	self.PhysCollides = CreatePhysCollidesFromModel( model )
 
 	-- Perhaps some of these are redundant
-	self:SetSolid(SOLID_VPHYSICS)
-	local phys = self:GetPhysicsObject()
-	if IsValid(phys) then
-		self:GetPhysicsObject():EnableMotion(false)
+	self:SetSolid( SOLID_VPHYSICS )
+	local phys = self:GetPhysicsObject( )
+	if IsValid( phys ) then
+
+		phys:EnableMotion( false )
+
 	end
 
-	self:EnableCustomCollisions(true)
+	self:EnableCustomCollisions( true )
+
 end
 
 
@@ -95,28 +107,33 @@ end
 --[[-------------------------------------------------------------------------
  Handles collisions against traces, includes player movement
 ---------------------------------------------------------------------------]]
-function ENT:TestCollision(startpos, delta, isbox, extents, mask)
+function ENT:TestCollision( startpos, delta, isbox, extents, mask )
+
 	if !self.PhysCollides then return end
-
 	-- Return nothing if our mask isn't involved with actual player collisions
-	if bit.band(mask, CONTENTS_PLAYERCLIP) == 0 then return end
-
+	if bit.band( mask, CONTENTS_PLAYERCLIP ) == 0 then return end
 	-- TraceBox expects the trace to begin at the center of the box, but TestCollision is very quite silly
 	local max = extents
 	local min = -extents
 	max.z = max.z - min.z
 	min.z = 0
 
-	for k, v in ipairs(self.PhysCollides) do
-		local hit, norm, frac = v:TraceBox(self:GetPos(), self:GetAngles(), startpos, startpos + delta, min, max)
-		
+	for k, v in ipairs( self.PhysCollides ) do
+
+		local hit, norm, frac = v:TraceBox( self:GetPos( ), self:GetAngles( ), startpos, startpos + delta, min, max )
+
 		if !hit then continue end
 
 		return {
+
 			HitPos = hit,
+
 			Normal = norm,
+
 			Fraction = frac,
+
 		}
+
 	end
 
 end
@@ -125,38 +142,48 @@ end
 --[[-------------------------------------------------------------------------
 Network position and angle to the client to overcome engine inaccuracies
 ---------------------------------------------------------------------------]]
-function ENT:NetworkAbsPosition()
+function ENT:NetworkAbsPosition( )
+
 	if CLIENT then return end
 
-	local ground = self:GetNW2Entity("Ground")
-	if !IsValid(ground) then return end
+	local ground = self:GetNW2Entity( "Ground" )
+	if !IsValid( ground ) then return end
 
-	local currPos = self:GetNW2Vector("GroundPos")
-	local currAng = self:GetNW2Vector("GroundAng")
+	local currPos = self:GetNW2Vector( "GroundPos" )
+	local currAng = self:GetNW2Vector( "GroundAng" )
 
-	local newPos = ground:GetPos()
-	local newAng = ground:GetAngles()
-
+	local newPos = ground:GetPos( )
+	local newAng = ground:GetAngles( )
 	-- Only network if there was a change
 	if !currPos or currPos != newPos then
-		self:SetNW2Vector("GroundPos", newPos)
+
+		self:SetNW2Vector( "GroundPos", newPos )
+
 	end
+
 	if !currAng or currAng != newAng then
-		self:SetNW2Angle("GroundAng", newAng)
+
+		self:SetNW2Angle( "GroundAng", newAng )
+
 	end
+
 end
 
 --[[-------------------------------------------------------------------------
 Walked on or not, we also wish to network the absolute position whenever it changes
 ---------------------------------------------------------------------------]]
-function ENT:CalcAbsolutePosition()
-	self:NetworkAbsPosition()
+function ENT:CalcAbsolutePosition( )
+
+	self:NetworkAbsPosition( )
+
 end
 
 
 --[[-------------------------------------------------------------------------
 Prevent Physgun pickups
 ---------------------------------------------------------------------------]]
-hook.Add("PhysgunPickup", "PreventPickups", function(ply, ent)
-	if ent:GetClass() == "prop_walker" then return false end
-end)
+hook.Add( "PhysgunPickup", "PreventPickups", function( ply, ent )
+
+	if ent:GetClass( ) == "prop_walker" then return false end
+
+end )
